5bde79e1129de502fad16024fcfa2c8b1af375fc
Marta Nara <marta.kos1230@gmail.com>
2022-01-18 15:05:04 +0100

    Add navigate to List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,10 +1,11 @@
 import styles from './List.module.scss';
-import { useSelector } from 'react-redux'
-import ColumnForm from '../ColumnForm/ColumnForm'
-import Column from '../Column/Column'
+import { useSelector } from 'react-redux';
+import ColumnForm from '../ColumnForm/ColumnForm';
+import Column from '../Column/Column';
 import { getColumnsByList, getListId } from '../../redux/store';
 import { useParams } from 'react-router';
-import SearchForm from '../SearchForm/SearchForm'
+import SearchForm from '../SearchForm/SearchForm';
+import { Navigate } from 'react-router-dom';
 
 const List = () => {
 
@@ -13,23 +14,23 @@ const List = () => {
   const listData = useSelector(state => getListId(state, listId))
   const columns = useSelector(state => getColumnsByList(state, listId))
 
-  return (
-    <div className={styles.list}>
-      <header className={styles.header}>
-        <h2 className={styles.title}>{listData.title}</h2>
-      </header>
-      <SearchForm />
-      <p className={styles.description}>{listData.description}</p>
-      <section className={styles.columns}>
-        {columns.map(column => <Column 
-          key={column.id}  
-          {...column} />
-        )}
-      </section>
-      <ColumnForm listId={listData.id}/>
-    </div>
-  );
-
+  if(!listData) return <Navigate to="/"/>
+    return (
+      <div className={styles.list}>
+        <header className={styles.header}>
+          <h2 className={styles.title}>{listData.title}</h2>
+        </header>
+        <SearchForm />
+        <p className={styles.description}>{listData.description}</p>
+        <section className={styles.columns}>
+          {columns.map(column => <Column 
+            key={column.id}  
+            {...column} />
+          )}
+        </section>
+        <ColumnForm listId={listData.id}/>
+      </div>
+    );
 };
 
 export default List;
